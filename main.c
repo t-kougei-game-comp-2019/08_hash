b3995607e43d671999bb22f549eca53f4a3c4add
Kuryu86 <Kuryu86+github@gmail.com>
2019-12-02 01:39:51 +0900

    テスト開始

--- main.c
+++ main.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
-#define STR_MAX 256
+#define STR_MAX 255
 #define HASH_MAX 10
-void HashFn(char *str,char Hash[][STR_MAX])
+void HashFn(char *str, char Hash[][STR_MAX])
 {
 	int sum = 0;
-	for (int i = 0; i < 7; i++)
+	for (int i = 0; i < STR_MAX; i++)
 	{
 		if (str[i] == '\n')
 			str[i] = '\0';
 		sum += str[i];
 	}
-	int HashNum = sum % 10;
-	for (; strcmp(Hash[HashNum], "\0") == 0; HashNum=(HashNum+1)%HASH_MAX)
-	{}
-	strcpy(Hash[HashNum], str);
+	int HashNum = sum % STR_MAX;
+	int count;
+	for (; strcmp(Hash[HashNum], "\0") != 0; HashNum = (HashNum + 1) % HASH_MAX)
+	{
+		count++;
+		if(count>HASH_MAX)
+			break;
+	}
+	if(count<HASH_MAX)
+		strcpy(Hash[HashNum], str);
+}
+void ShowHash(char Hash[][STR_MAX])
+{
+	for (size_t i = 0; i < STR_MAX; i++)
+	{
+		printf("%s", Hash[i]);
+		if (i + 1 == 10)
+			printf("\n");
+		else
+			printf(",");
+	}
 }
 int main(int argc, char *argv[])
 {
@@ -23,12 +40,11 @@ int main(int argc, char *argv[])
 	char str[STR_MAX];
 	while (fgets(str, sizeof(str), stdin))
 	{
-		int sum = 0;
 		if (strcmp(str, "\0") != 0)
 			HashFn(str, Hash);
-		
-		
+		else
+			ShowHash(Hash);
 	}
-	
+
 	return 0;
 }
