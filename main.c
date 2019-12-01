df02a863d64ec3bbeea44b230c4004dc8bf5dcc8
Kuryu86 <Kuryu86+github@gmail.com>
2019-12-02 01:20:53 +0900

    ハッシュ関数の定義

--- main.c
+++ main.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) 
+#define STR_MAX 256
+#define HASH_MAX 10
+void HashFn(char *str,char Hash[][STR_MAX])
 {
-    char str[7];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
-
-    return 0;
+	int sum = 0;
+	for (int i = 0; i < 7; i++)
+	{
+		if (str[i] == '\n')
+			str[i] = '\0';
+		sum += str[i];
+	}
+	int HashNum = sum % 10;
+	for (; strcmp(Hash[HashNum], "\0") == 0; HashNum=(HashNum+1)%HASH_MAX)
+	{}
+	strcpy(Hash[HashNum], str);
+}
+int main(int argc, char *argv[])
+{
+	char Hash[10][STR_MAX] = {};
+	char str[STR_MAX];
+	while (fgets(str, sizeof(str), stdin))
+	{
+		int sum = 0;
+		if (strcmp(str, "\0") != 0)
+			HashFn(str, Hash);
+		
+		
+	}
+	
+	return 0;
 }
