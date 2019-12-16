da554bfea7a5ef2ce98c4b0151ae98f181ba26bf
Kuryu86 <Kuryu86+github@gmail.com>
2019-12-16 18:46:59 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
+#define  _CRT_SECURE_NO_WARNINGS
+
 #define STR_MAX 512
 #define HASH_MAX 10
 int IsHashEmpty[HASH_MAX];
-void HashFn(int HashNum, char Hash[][STR_MAX]){
-	while (IsHashEmpty[HashNum]!= 0)
+void HashFn(int HashNum, char Hash[][STR_MAX]) {
+	while (IsHashEmpty[HashNum] != 0)
 	{
 		HashNum = (HashNum + 1) % HASH_MAX;
 	}
 	//strncpy(Hash[HashNum], tmp, STR_MAX);
-        IsHashEmpty[HashNum]= 1;
+	IsHashEmpty[HashNum] = 1;
 }
 void ShowHash(char Hash[][STR_MAX])
 {
@@ -26,36 +28,36 @@ void ShowHash(char Hash[][STR_MAX])
 }
 int main(int argc, char *argv[])
 {
-	char Hash[HASH_MAX][STR_MAX] = {};
+	char Hash[HASH_MAX][STR_MAX];
 	char str[STR_MAX];
-	for(int i=0;i<HASH_MAX;i++)
-            IsHashEmpty[i]=0;
+	for (int i = 0; i<HASH_MAX; i++)
+		IsHashEmpty[i] = 0;
 	while (fgets(str, sizeof(str), stdin))
 	{
-		if (str[0]!='\n')
-		{          
+		if (str[0] != '\n')
+		{
 			int sum = 0;
 			for (int i = 0; i < STR_MAX; i++)
 			{
 				if (str[i] == '\n')
 				{
-						str[i]='\0';
-						break;
+					str[i] = '\0';
+					break;
 				}
 				sum += str[i];
-				sum%=HASH_MAX;
+				sum %= HASH_MAX;
 			}
-			int HashNum=sum;
-			while (IsHashEmpty[HashNum]!= 0)
+			int HashNum = sum;
+			while (IsHashEmpty[HashNum] != 0)
 				HashNum = (HashNum + 1) % HASH_MAX;
-			strncpy(Hash[HashNum], str, STR_MAX);
-			IsHashEmpty[HashNum]= 1;
+			strncpy_s(Hash[HashNum], sizeof(Hash[HashNum]), str, STR_MAX);
+			IsHashEmpty[HashNum] = 1;
 		}
 		else
 		{
 			for (size_t i = 0; i < HASH_MAX; i++)
 			{
-				if (IsHashEmpty[i]!= 0)
+				if (IsHashEmpty[i] != 0)
 					printf("%s", Hash[i]);
 				if (i + 1 == 10)
 					printf("\n");
@@ -66,4 +68,3 @@ int main(int argc, char *argv[])
 	}
 	return 0;
 }
-	
